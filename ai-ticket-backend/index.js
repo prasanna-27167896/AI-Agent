a4bd1271c5113950ef4ae3abcd33432df0c47afa
prasanna kumar <vprasannakumar2716@gmail.com>
2025-06-24 18:09:22 +0530

    final commited

--- ai-ticket-backend/index.js
+++ ai-ticket-backend/index.js
@@ -12,7 +12,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://tickzy-ai-agent.netlify.app/", // Replace with your Netlify URL
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/auth", userRoutes);
